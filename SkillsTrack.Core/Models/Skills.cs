4e8bae1072890da75556a253481deda23512eac9
jakub <jakubstankowski@protonmail.com>
2021-12-17 15:25:42 +0100

    Add Core project, and Skills model

---
+++ SkillsTrack.Core/Models/Skills.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsTrack.Core.Models
+{
+    public class Skills
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
