e7d42dd40a55a3bf19e522a1408045bbc7cb1d92
jakub <jakubstankowski@protonmail.com>
2021-12-17 15:44:38 +0100

    Success add first skills controller

---
+++ SkillsTrack.API/Controllers/SkillsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkillsTrack.Core.Interfaces;
+
+namespace SkillsTrack.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly ISkillsService _skillsService;
+
+        public SkillsController(ISkillsService skillsService)
+        {
+            _skillsService = skillsService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllSkills()
+        {
+            var skills = _skillsService.GetAllSkills();
+
+            return Ok(skills);
+        }
+
+    }
+}
