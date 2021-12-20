8262d15a08baef150ec04cec0c26ac37afe387c4
jakub <jakubstankowski@protonmail.com>
2021-12-20 19:15:42 +0100

    Redesign basic client project layout

--- SkillsTrack.Client/Services/SkillsService.cs
+++ SkillsTrack.Client/Services/SkillsService.cs
@@ -16,11 +16,15 @@ namespace SkillsTrack.Client.Services
 
         public List<Skill> Skills { get; set; } = new List<Skill>();
 
+        public event Action OnChange;
+
+
         public async Task<List<Skill>> CreateSkillAsync(Skill skill)
         {
             var result = await _httpClient.PostAsJsonAsync<Skill>($"api/skills", skill);
             Skills = await result.Content.ReadFromJsonAsync<List<Skill>>();
-            // OnChange.Invoke();
+
+            OnChange.Invoke();
             return Skills;
         }
 
